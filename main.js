608cf9567c2b264064cbc56b37aa147d17ce413b
Dioma <dioma@gmail.com>
2020-01-18 15:27:20 +0200

    added anchors and js scripts

--- main.js
+++ main.js
@@ -12,26 +12,38 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("sliders");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < slides.length; i++) {
-      slides[i].className = slides[i].className.replace(" active", "");
+    slides[i].className = slides[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
+};
+
+var boxes = document.getElementsByClassName("buying__box");
+
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("click", changeBox);
+  function changeBox() {
+    var currentBox = document.getElementsByClassName("active-box");
+    currentBox[0].className = currentBox[0].className.replace("active-box", " ");
+    this.className += " active-box";
+  };
+};
+
+var links = document.getElementsByClassName("link");
+
+for (var j = 0; j < links.length; j++) {
+  links[j].addEventListener("click", changeLink);
+  function changeLink() {
+    var currentLink = document.getElementsByClassName("active-link");
+    currentLink[0].className = currentLink[0].className.replace("active-link", " ");
+    this.className += " active-link";
+  };
 };
 
 
-var wrap = document.getElementsByClassName("buying__wrap");
-var boxes = document.getElementsByClassName("buying__box");
-
-for (var j = 0; j < boxes.length; j++) {
-  boxes[j].addEventListener("click", function() {
-    var currentBox = document.getElementsByClassName("active-box");
-    currentBox[0].className = currentBox[0].className.replace(" active-box", "");
-    this.className += " active-box";
-  });
-}
 
